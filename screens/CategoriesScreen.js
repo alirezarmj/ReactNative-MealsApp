d29d9c97ef0bccb9105bc7eedd0003e54d332001
alirezarmj <a.rashidi.m78@gmail.com>
2024-01-05 08:55:19 +0330

    Working with route params

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -5,7 +5,7 @@ import CategoryGridTile from "../components/CategoryGridTile";
 function CategoriesScreen({ navigation }) {
   function renderCategoryItem(itemdata) {
     function handlePress() {
-      navigation.navigate("MealsOverview");
+      navigation.navigate("MealsOverview", { categoryId: itemdata.item.id });
     }
     return (
       <CategoryGridTile
